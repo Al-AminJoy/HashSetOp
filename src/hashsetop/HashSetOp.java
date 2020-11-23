0a5312eda250a5f817622193ae4586d9655d5db0
Al-AminJoy <alaminislam3555@gmail.com>
2020-11-23 21:48:31 +0600

    Complete

---
+++ src/hashsetop/HashSetOp.java
@@ -0,0 +1,18 @@
+package hashsetop;
+
+import java.util.HashSet;
+
+public class HashSetOp {
+
+  
+    public static void main(String[] args) {
+    HashSet<Integer> hash=new HashSet<>();
+    hash.add(1);
+    hash.add(2);
+    hash.add(3);
+    System.out.println(hash);
+    //remove,clear or other operation are same as ArrayList
+    
+    }
+    
+}
